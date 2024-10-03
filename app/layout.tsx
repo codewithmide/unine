1cec8b2c0bb7d2a4cb9cc4264b1e88d2a7d378b3
Olumide Micheal <olumidemicheal7@gmail.com>
2024-10-03 01:28:44 +0100

    added opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Unine",
   description: "More Payment Options Better than Cash",
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
+  twitter: {
+    title: "Unine",
+    description:
+      "More Payment Options Better than Cash",
+    images: "/twitter-image.png",
+    creator: "@codewithmide",
+  },
 };
 
 export default function RootLayout({
